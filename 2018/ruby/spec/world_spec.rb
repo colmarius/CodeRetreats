ae28308acab22d2c2cba748b4da5695b981dc34c
Marius Colacioiu <marius.colacioiu@gmail.com>
2018-11-17 14:49:52 +0100

    world can evolve 3

--- 2018/ruby/spec/world_spec.rb
+++ 2018/ruby/spec/world_spec.rb
@@ -2,18 +2,17 @@ require 'spec_helper'
 require 'world'
 
 RSpec.describe World do
+  let(:world) { World.new }
   it 'should not raise error' do
     expect { World.new }.not_to raise_error
   end
 
   it 'with one cell which is alive' do
-    world = World.new
     cell = world.state
     expect(cell.is_alive?).to eq(true)
   end
 
   it 'can evolve' do
-    world = World.new
 
   end
 end
