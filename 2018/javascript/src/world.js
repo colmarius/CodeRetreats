8d2658c8c9a703ff3832342abba3ccdfabdfeae7
Marius Colacioiu <marius.colacioiu@gmail.com>
2018-11-17 12:50:24 +0100

    Done session 3

--- 2018/javascript/src/world.js
+++ 2018/javascript/src/world.js
@@ -1,3 +1,29 @@
-const world = () => 'hello world'
+class Cell {
+  constructor() {
+    this.state = 'alive'
+  }
 
-module.exports = world
+  isAlive() {
+    return this.state === 'alive'
+  }
+
+  die() {
+    this.state = 'dead'
+  }
+}
+
+class World {
+  constructor(size) {
+    this.size = size
+    this.state = new Cell()
+  }
+
+  evolve() {
+    this.state.die()
+  }
+}
+
+module.exports = {
+  Cell,
+  World,
+}
--- 2018/javascript/src/world.test.js
+++ 2018/javascript/src/world.test.js
@@ -1,5 +1,30 @@
-const world = require('./world')
+const { World, Cell } = require('./world')
 
-test('hellow world', () => {
-  expect(world()).toEqual('hello world')
+test('create world with size', () => {
+  const world = new World(1)
+  expect(world.size).toBe(1)
+})
+
+test('world with 1 cell is alive at beginning', () => {
+  const world = new World(1)
+  const cell = world.state
+  expect(cell.isAlive()).toEqual(true)
+})
+
+test('world with alive cell dies in next evolution', () => {
+  const world = new World(1)
+  world.evolve()
+  const cell = world.state
+  expect(cell.isAlive()).toEqual(false)
+})
+
+test('create cell is alive by default', () => {
+  const cell = new Cell()
+  expect(cell.isAlive()).toBe(true)
+})
+
+test('alive cell can die', () => {
+  const cell = new Cell()
+  cell.die()
+  expect(cell.isAlive()).toBe(false)
 })
